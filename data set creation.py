70e0f3983feb8d6c4b529b7614816561d954631a
rohith vishnu <karanamrohith@gmail.com>
2018-01-13 23:53:04 +0530

    dataset creation

---
+++ data set creation.py
@@ -0,0 +1,12 @@
+from collections import OrderedDict
+
+with open("urls.txt","r") as f:
+    content = f.readlines()
+websites=[x.split() for x in content]
+final_urls=[]
+for website in websites:
+    url=website[0].split('/')
+    final_urls.append(url[2])
+final_urls_1=list(set(final_urls))
+print(final_urls_1)
+
